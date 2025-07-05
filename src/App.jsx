908f0885e62b2be1cbd34bf98d2c25b927ceec74
Rasika <rassiipowar@gmail.com>
2025-07-05 13:07:23 +0530

    Fix: add missing react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-
 import "./styles/Pages.css";
+import './App.css'
 import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
+
+
+import { useState } from "react";
+import DeveloperInfoPopup from "./components/Developerinfo/DeveloperInfoPopup";
+import HomePage from "./pages/Homepage";
+import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
 import AdmissionPage from "./pages/AdmissionPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import HomePage from "./pages/Homepage";
-import ContactPage from "./pages/ContactPage";
-import { useState } from "react";
-import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
-
-
-
-
 
 function App() {
   const [showPopup, setShowPopup] = useState(true);
   const handleClosePopup = () => {
     setShowPopup(false);
   };
+
   return (
-    <>
-    <div>
+    <> <div>
         {/* Your main application content */}
         <DeveloperInfoPopup
           show={showPopup}
@@ -35,13 +33,12 @@ function App() {
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
-
     <Router>
       <div className="main-layout">
         <Header />
         <div className="content">
           <Routes>
-             <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/courses" element={<CoursesPage />} />
