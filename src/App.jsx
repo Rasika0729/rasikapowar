8378699a0a87edee4dd0a8ebbd6cbd41f083c060
Rasika <rassiipowar@gmail.com>
2025-07-16 22:20:55 +0530

    Fix case-sensitive import path for HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,12 @@ import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
 
 import { useState } from "react";
 import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
-import HomePage from "./pages/Homepage";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
 import AdmissionPage from "./pages/AdmissionPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const [showPopup, setShowPopup] = useState(true);
@@ -38,7 +38,7 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<HomePage/>} />
             <Route path="/contact" element={<ContactPage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/courses" element={<CoursesPage />} />
